4a7f937713396f322dfb52856dae54b27eda48d3
kao <kao@aklamio.com>
2018-06-21 11:10:06 +0200

    Add fetch single question action creator

--- src/actions/api.js
+++ src/actions/api.js
@@ -2,7 +2,8 @@ import fetch from 'cross-fetch';
 import {
   FETCH_QUESTIONS_SUCCESS,
   QUESTIONS_HAVE_ERROR,
-  QUESTIONS_LOADING
+  QUESTIONS_LOADING,
+  FETCH_QUESTION
 } from './types';
 
 const ROOT_URL = 'https://polls.apiblueprint.org/questions';
@@ -45,5 +46,34 @@ export function questionsLoading(bool) {
   return {
     type: QUESTIONS_LOADING,
     isLoading: bool
+  };
+}
+
+export function fetchSingleQuestion(id) {
+  let url = `${ROOT_URL}/${id}`;
+
+  return dispatch => {
+    dispatch(questionsLoading(true));
+    fetch(url)
+      .then( resp => {
+        if (resp.status >= 400) {
+          throw new Error('Bad response from server');
+        }
+        return resp.json();
+      })
+      .then( resp => {
+        dispatch(fetchSingleQuestionSuccess(resp));
+        dispatch(questionsLoading(false));
+      })
+      .catch(() => {
+        dispatch(questionsHaveError(true));
+      });
+  }
+}
+
+export function fetchSingleQuestionSuccess(question) {
+  return {
+    type: FETCH_QUESTION,
+    question
   };
 }
