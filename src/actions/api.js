a06c83717dff78c955204b9aa035698e3397bcdf
kao <kao@aklamio.com>
2018-06-21 16:41:07 +0200

    Refactor api call action creators and add post request for voting

--- src/actions/api.js
+++ src/actions/api.js
@@ -1,79 +1,58 @@
-import fetch from 'cross-fetch';
-import {
-  FETCH_QUESTIONS_SUCCESS,
-  QUESTIONS_HAVE_ERROR,
-  QUESTIONS_LOADING,
-  FETCH_QUESTION
-} from './types';
+import axios from 'axios';
+import {FETCH_QUESTIONS_SUCCESS, QUESTIONS_HAVE_ERROR, QUESTIONS_LOADING, FETCH_QUESTION, VOTE_ON_CHOICE} from './types';
 
-const ROOT_URL = 'https://polls.apiblueprint.org/questions';
+const ROOT_URL = 'https://polls.apiblueprint.org/';
 
 export function fetchQuestions() {
+  let url = `${ROOT_URL}/questions`;
+  let request = axios.get(url);
+
   return dispatch => {
     dispatch(questionsLoading(true));
-    fetch(ROOT_URL)
-      .then( resp => {
-        if (resp.status >= 400) {
-          throw new Error('Bad response from server');
-        }
-        return resp.json();
-      })
-      .then( resp => {
-        dispatch(fetchQuestionsSuccess(resp));
-        dispatch(questionsLoading(false));
-      })
-      .catch(() => {
-        dispatch(questionsHaveError(true));
-      });
+    request.then(resp => {
+      dispatch(fetchQuestionsSuccess(resp.data));
+      dispatch(questionsLoading(false));
+    }).catch(() => {
+      dispatch(questionsHaveError(true));
+    });
   }
 }
 
 export function fetchQuestionsSuccess(questions) {
-  return {
-    type: FETCH_QUESTIONS_SUCCESS,
-    questions
-  };
+  return {type: FETCH_QUESTIONS_SUCCESS, questions};
 }
 
 export function questionsHaveError(bool) {
-  return {
-    type: QUESTIONS_HAVE_ERROR,
-    hasError: bool
-  };
+  return {type: QUESTIONS_HAVE_ERROR, hasError: bool};
 }
 
 export function questionsLoading(bool) {
-  return {
-    type: QUESTIONS_LOADING,
-    isLoading: bool
-  };
+  return {type: QUESTIONS_LOADING, isLoading: bool};
 }
 
 export function fetchSingleQuestion(id) {
-  let url = `${ROOT_URL}/${id}`;
+  let url = `${ROOT_URL}/questions/${id}`;
+  let request = axios.get(url);
 
   return dispatch => {
     dispatch(questionsLoading(true));
-    fetch(url)
-      .then( resp => {
-        if (resp.status >= 400) {
-          throw new Error('Bad response from server');
-        }
-        return resp.json();
-      })
-      .then( resp => {
-        dispatch(fetchSingleQuestionSuccess(resp));
-        dispatch(questionsLoading(false));
-      })
-      .catch(() => {
-        dispatch(questionsHaveError(true));
-      });
+    request.then(resp => {
+      dispatch(fetchSingleQuestionSuccess(resp.data));
+      dispatch(questionsLoading(false));
+    }).catch(() => {
+      dispatch(questionsHaveError(true));
+    });
   }
 }
 
 export function fetchSingleQuestionSuccess(question) {
+  return {type: FETCH_QUESTION, question};
+}
+
+export function postVote(choice, callback) {
+  let request = axios.post(`${ROOT_URL}${choice}`).then(() => callback());
   return {
-    type: FETCH_QUESTION,
-    question
+    type: VOTE_ON_CHOICE,
+    payload: request
   };
 }
