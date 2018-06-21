921bc89d3700e487120329c3932c70f19c28f0cf
kao <kao@aklamio.com>
2018-06-21 09:43:19 +0200

    Render QuestionItem in QuestionList

--- src/containers/QuestionsList.js
+++ src/containers/QuestionsList.js
@@ -1,28 +1,68 @@
+import _ from 'lodash';
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import {fetchQuestions} from '../actions/api';
-import _ from 'lodash';
+import {Grid, Row, Col} from 'react-bootstrap';
+import QuestionItem from '../components/QuestionItem';
 import PropTypes from 'prop-types';
 
 class QuestionsList extends Component {
   componentDidMount() {
-    this.props.fetchQuestions();
+    this
+      .props
+      .fetchQuestions();
+  }
+
+  renderQuestionItem() {
+    const questions = this.props.questions;
+
+    return _.map(questions, (item, index) => {
+      return <QuestionItem key={index} title={item.question} published_at={item.published_at} choices={item.choices.length}/>;
+    });
+  }
+
+  renderList() {
+    if (this.props.isLoading) {
+      return <p>Loading...</p>;
+    } else {
+      return this.renderQuestionItem();
+    }
   }
 
   render() {
     return (
-      <div>Show some list</div>
+      <React.Fragment>
+        <header>
+          <Grid>
+            <Row>
+              <Col xs={12}>
+                <h1>Questions</h1>
+              </Col>
+            </Row>
+          </Grid>
+        </header>
+        <section>
+          <Grid>
+            <Row>
+              {this.renderList()}
+            </Row>
+          </Grid>
+        </section>
+      </React.Fragment>
     );
   }
 }
 
-function mapStateToProps({questions}) {
-  return {questions}
+function mapStateToProps({ questions, questionsLoading }) {
+  return {
+    questions,
+    isLoading: questionsLoading
+  }
 }
 
 QuestionsList.propTypes = {
-  questions: PropTypes.object.isRequired,
+  isLoading: PropTypes.bool.isRequired,
   fetchQuestions: PropTypes.func.isRequired
 };
 
-export default connect(mapStateToProps, { fetchQuestions })(QuestionsList);
+export default connect(mapStateToProps, {fetchQuestions})(QuestionsList);
