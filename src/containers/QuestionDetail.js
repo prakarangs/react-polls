c3638f187c03a840577efa283c41baa079bc1b93
kao <kao@aklamio.com>
2018-06-21 16:43:09 +0200

    Add form submit handling function for posting vote

--- src/containers/QuestionDetail.js
+++ src/containers/QuestionDetail.js
@@ -1,27 +1,67 @@
-import React, {Component} from 'react';
 import _ from 'lodash';
+import React, {Component} from 'react';
 import {connect} from 'react-redux';
-import {fetchSingleQuestion} from '../actions/api';
-import {Grid, Row, Col, ListGroup, ListGroupItem} from 'react-bootstrap';
+import {Link} from 'react-router-dom';
+import { fetchSingleQuestion, postVote } from '../actions/api';
+import {
+  Grid,
+  Row,
+  Col,
+  ListGroup,
+  ListGroupItem,
+  ControlLabel,
+  Button
+} from 'react-bootstrap';
 
 class QuestionDetail extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      total: 0,
+      choice: {
+        title: 'no selected choice',
+        url: null
+      }
+    };
+
+    this.handleVoteSubmit = this.handleVoteSubmit.bind(this);
+  }
 
   componentDidMount() {
-    const { questionId } = this.props.match.params;
-    this
-      .props
-      .fetchSingleQuestion(questionId);
+    const {questionId} = this.props.match.params;
+    this.props.fetchSingleQuestion(questionId);
+  }
+
+  handleChoiceOnClick({choice, url}) {
+    console.log(this.props);
+    this.setState({
+      choice: {
+        title: choice,
+        url: url
+      }
+    });
+  }
+
+  handleVoteSubmit(event) {
+    event.preventDefault();
+    this.props.postVote(this.state.choice.url, () => {
+      this.props.history.push(`${this.props.match.url}/voted`);
+    });
   }
 
   renderChoices() {
-    const { choices } = this.props.activeQuestion;
+    const {choices} = this.props.activeQuestion;
     return _.map(choices, (choice, index) => {
-      console.log(choice);
       return (
-        <ListGroupItem key={index}>
+        <ListGroupItem
+          key={index}
+          onClick={(event) => {
+            event.preventDefault();
+            this.handleChoiceOnClick(choice);
+          }}>
           <h3>Choice: {choice.choice}</h3>
           <span>Number of votes: {choice.votes}</span>
-          <span></span>
         </ListGroupItem>
       );
     });
@@ -38,9 +78,13 @@ class QuestionDetail extends Component {
           <header>
             <Grid>
               <Row>
-                <Col sm={12}>
+                <Col md={6}>
                   <h1>Questions Detail</h1>
-                  <h2>{question}</h2>
+                </Col>
+                <Col md={6}>
+                <Link to={'/'}>
+                  <Button>Back to all questions</Button>
+                  </Link>
                 </Col>
               </Row>
             </Grid>
@@ -48,10 +92,16 @@ class QuestionDetail extends Component {
           <section>
             <Grid>
               <Row>
+                <h2>{question}</h2>
                 <Col sm={12}>
-                  <ListGroup>
-                    {this.renderChoices()}
-                  </ListGroup>
+                  <form onSubmit={this.handleVoteSubmit}>
+                    <ListGroup>
+                      {this.renderChoices()}
+                    </ListGroup>
+                    {this.state.choice.url}
+                    <ControlLabel>Your vote: <span>{this.state.choice.title}</span></ControlLabel>
+                    <Button type="submit">Save vote</Button>
+                  </form>
                 </Col>
               </Row>
             </Grid>
@@ -61,7 +111,9 @@ class QuestionDetail extends Component {
     }
   }
 }
+
 function mapStateToProps({activeQuestion, questionsLoading}) {
   return {activeQuestion, isLoading: questionsLoading}
 }
-export default connect(mapStateToProps, {fetchSingleQuestion})(QuestionDetail);
+
+export default connect(mapStateToProps, {fetchSingleQuestion, postVote})(QuestionDetail);
