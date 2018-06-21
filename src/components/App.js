05991c840bd3d6ea90eebad5144a592a1cd978ca
kao <kao@aklamio.com>
2018-06-21 08:13:07 +0200

    Revert "Add react router to App"
    This reverts commit 816dd241d4077ee7ef553b200333ed958a7418ae.

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,9 @@
 import React from 'react';
-import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
-import QuestionsList from '../containers/QuestionsList';
 
 const App = () => (
-  <BrowserRouter>
-    <React.Fragment>
-    <Route exact path="/" render={() => <Redirect to="/questions" />} />
-    <Switch>
-      <Route path="/questions" component={QuestionsList} />
-    </Switch>
-    </React.Fragment>
-  </BrowserRouter>
+  <div>
+    Hello World
+  </div>
 )
 
 export default App;
