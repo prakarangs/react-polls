021559ab101f31d9682d58b555aa113fe0321c14
kao <kao@aklamio.com>
2018-06-22 05:55:22 +0200

    Calculate percentage of votes

--- src/containers/QuestionDetail.js
+++ src/containers/QuestionDetail.js
@@ -2,7 +2,7 @@ import _ from 'lodash';
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import {Link} from 'react-router-dom';
-import { fetchSingleQuestion, postVote } from '../actions/api';
+import {fetchSingleQuestion, postVote} from '../actions/api';
 import {
   Grid,
   Row,
@@ -10,8 +10,11 @@ import {
   ListGroup,
   ListGroupItem,
   ControlLabel,
-  Button
+  Button,
+  ProgressBar
 } from 'react-bootstrap';
+import PropTypes from 'prop-types';
+import '../styles/detail.css';
 
 class QuestionDetail extends Component {
   constructor(props) {
@@ -25,16 +28,24 @@ class QuestionDetail extends Component {
       }
     };
 
-    this.handleVoteSubmit = this.handleVoteSubmit.bind(this);
+    this.handleVoteSubmit = this
+      .handleVoteSubmit
+      .bind(this);
   }
 
   componentDidMount() {
     const {questionId} = this.props.match.params;
-    this.props.fetchSingleQuestion(questionId);
+
+    this.props.fetchSingleQuestion(questionId, () => {
+      let total = 0;
+      _.map(this.props.activeQuestion.choices, (choice) => {
+        total = total + choice.votes;
+      });
+      this.setState({total: total});
+    });
   }
 
   handleChoiceOnClick({choice, url}) {
-    console.log(this.props);
     this.setState({
       choice: {
         title: choice,
@@ -50,6 +61,10 @@ class QuestionDetail extends Component {
     });
   }
 
+  calculatePercentage(votes) {
+    return Math.floor((votes / this.state.total) * 100);
+  }
+
   renderChoices() {
     const {choices} = this.props.activeQuestion;
     return _.map(choices, (choice, index) => {
@@ -57,34 +72,50 @@ class QuestionDetail extends Component {
         <ListGroupItem
           key={index}
           onClick={(event) => {
-            event.preventDefault();
-            this.handleChoiceOnClick(choice);
-          }}>
-          <h3>Choice: {choice.choice}</h3>
-          <span>Number of votes: {choice.votes}</span>
+          event.preventDefault();
+          this.handleChoiceOnClick(choice);
+        }}>
+          <div className="choices_wrapper">
+            <div className="choices_inner choices_inner--title">
+              <small>Choice</small>
+              <h3>{choice.choice}</h3>
+            </div>
+            <div className="choices_inner">
+              <small>Number of votes</small>
+              <strong>{choice.votes}</strong>
+            </div>
+            <div className="choices_inner choices_inner--percentage">
+              <small>Percentagae</small>
+              <strong>{String(this.calculatePercentage(choice.votes))}</strong>
+            </div>
+            <div className="choices_inner choices_inner--progress">
+              <ProgressBar bsStyle="info" now={this.calculatePercentage(choice.votes)} />
+            </div>
+          </div>
         </ListGroupItem>
       );
     });
+
   }
 
   render() {
-    const { question } = this.props.activeQuestion;
+    const {question} = this.props.activeQuestion;
 
     if (this.props.isLoading) {
       return <p>Loading...</p>;
     } else {
       return (
         <React.Fragment>
-          <header>
+          <header className="header_detail">
             <Grid>
               <Row>
-                <Col md={6}>
-                  <h1>Questions Detail</h1>
-                </Col>
-                <Col md={6}>
-                <Link to={'/'}>
-                  <Button>Back to all questions</Button>
-                  </Link>
+                <Col sm={12}>
+                  <h1>{question}</h1>
+                  <div className="cta_back">
+                    <Link to={'/'}>
+                      <Button bsSize="small">Back to all questions</Button>
+                    </Link>
+                  </div>
                 </Col>
               </Row>
             </Grid>
@@ -92,15 +123,20 @@ class QuestionDetail extends Component {
           <section>
             <Grid>
               <Row>
-                <h2>{question}</h2>
                 <Col sm={12}>
+                  <p>Click to select your choice</p>
                   <form onSubmit={this.handleVoteSubmit}>
                     <ListGroup>
                       {this.renderChoices()}
                     </ListGroup>
-                    {this.state.choice.url}
-                    <ControlLabel>Your vote: <span>{this.state.choice.title}</span></ControlLabel>
-                    <Button type="submit">Save vote</Button>
+                    <div className="clearfix">
+                      <ControlLabel>
+                        <small>You selected</small>
+                        <span className="choice--selected">{this.state.choice.title}</span>
+                      </ControlLabel>
+                      <Button className="cta_submit--vote" bsStyle="warning" type="submit">Save vote</Button>
+                    </div>
+
                   </form>
                 </Col>
               </Row>
@@ -115,5 +151,14 @@ class QuestionDetail extends Component {
 function mapStateToProps({activeQuestion, questionsLoading}) {
   return {activeQuestion, isLoading: questionsLoading}
 }
+
+QuestionDetail.propTypes = {
+  isLoading: PropTypes.bool.isRequired,
+  history: PropTypes.object.isRequired,
+  match: PropTypes.object.isRequired,
+  activeQuestion: PropTypes.object.isRequired,
+  fetchSingleQuestion: PropTypes.func.isRequired,
+  postVote: PropTypes.func.isRequired
+};
 
 export default connect(mapStateToProps, {fetchSingleQuestion, postVote})(QuestionDetail);
